58d8a63e9ecdf13a99bd14af8cce3a0cb99a194e
fgalan-r <tremerin13@gmail.com>
2023-09-04 03:36:42 +0200

    open window

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -3,9 +3,32 @@
 
 # include "../lib/libft/inc/libft.h"
 # include "../lib/MLX42/include/MLX42/MLX42.h"
+# include <unistd.h>
+# include <stdlib.h>
 # include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <limits.h>
 # include <math.h>
 
+typedef struct s_point
+{
+	float		x;
+	float		y;
+}	t_point;
 
+typedef struct s_data
+{
+	mlx_t		*mlx;
+	mlx_image_t	*screen;
+	t_point		ply_pos;
+	float		ply_angle;
+	float		ply_speed;
+	char		*map_name;
+	char		**map;
+}	t_data;
+
+//provisionales
+void	create_test_map(t_data *data, int width, int heigth);
 
 #endif
--- src/main/main.c
+++ src/main/main.c
@@ -2,8 +2,15 @@
 
 int main(int argc, char **argv)
 {
-
-
-
+	(void)argc;
+	(void)argv;
+	t_data data;
+	int		width = 640;
+	int		height = 640;
+	
+	data.mlx = mlx_init(width, height, "cub3D", true);
+	create_test_map(&data, 10, 10);
+	mlx_loop(data.mlx);
+	mlx_terminate(data.mlx);
 	return (0);
 }
