a62af570b2ca0d0c156c436ef96143946aeb25fe
fgalan-r <tremerin13@gmail.com>
2023-09-06 17:14:02 +0200

    draw player

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -19,22 +19,35 @@ typedef struct s_point
 
 typedef struct s_data
 {
+	//ventana
 	mlx_t		*mlx;
 	mlx_image_t	*screen;
+	//player
 	t_point		ply_pos;
+	t_point		ply_cell;
 	float		ply_angle;
 	float		ply_speed;
-	int			x_axis; 	//right - left
-	int			y_axis; 	//down - up
-	int			turn_dir; 	//right - left
+	int			ply_x_axis; 	//right - left
+	int			ply_y_axis; 	//down - up
+	int			ply_turn_dir; 	//right - left
+	//mapa
+	mlx_image_t	*map_image;
 	char		*map_name;
 	char		**map;
+	//png
+	mlx_image_t	*wall;
 }	t_data;
 
 //provisionales
 void	create_test_map(t_data *data, int width, int heigth);
+void    draw_map(t_data *data, int width, int heigth, int size);
 
 //dibujo
 void    draw_line(mlx_image_t *image, t_point begin, t_point end, int color);
 void    draw_circle(mlx_image_t *image, t_point center, int radius, int color);
+void	clear_image(mlx_image_t *image);
+void	draw_player(t_data *data);
+
+//hook
+void	hook(void *param);
 #endif
--- src/main/main.c
+++ src/main/main.c
@@ -1,15 +1,40 @@
 #include "../../inc/cub3D.h"
 
+void	init_data(t_data *data)
+{
+	int				width;
+	int				height;
+	mlx_texture_t	*texture;
+
+	width = 640;
+	height = 640;
+	//create map
+	create_test_map(data, 10, 10);
+	//player data
+	data->ply_pos.x = 100;
+	data->ply_pos.y = 100;
+	data->ply_angle = 45;
+	data->ply_speed = 3;
+	//open window
+	data->mlx = mlx_init(width, height, "cub3D", true);
+	//creating images
+	texture = mlx_load_png("./png/wall.png");
+	data->wall = mlx_texture_to_image(data->mlx, texture);
+	//draw map 2d
+	draw_map(data, 10, 10, 64);
+	//create player screen
+	data->screen = mlx_new_image(data->mlx, width, height);
+	mlx_image_to_window(data->mlx, data->screen, 0, 0);
+}
+
 int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
 	t_data data;
-	int		width = 640;
-	int		height = 640;
-	
-	data.mlx = mlx_init(width, height, "cub3D", true);
-	create_test_map(&data, 10, 10);
+
+	init_data(&data);
+	mlx_loop_hook(data.mlx, &hook, &data);
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
 	return (0);
