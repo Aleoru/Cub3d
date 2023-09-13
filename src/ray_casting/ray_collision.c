6009ba3a72696dade280eb8a6a87e7091104f866
tremerin <tremerin13@hotmail.com>
2023-09-13 19:32:47 +0200

    v_collision

--- src/ray_casting/ray_collision.c
+++ src/ray_casting/ray_collision.c
@@ -6,26 +6,32 @@
 /*   By: fgalan-r <fgalan-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 03:49:41 by fgalan-r          #+#    #+#             */
-/*   Updated: 2023/09/13 04:20:42 by fgalan-r         ###   ########.fr       */
+/*   Updated: 2023/09/13 19:17:35 by fgalan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
 
-static float	ray_v_collision(t_data, int ray)
+static float	ray_v_collision(t_data *data, int ray)
+{
+	float	dist;
+	float	h;
+	int		next;
+
+	if (data->rays[ray].y_dir == 1)
+	{
+		next = (data->ply_pos.y / data->map_cell_size) * data->map_cell_size;
+		h = data->ply_pos.y;
+	}
+	return (dist);
+}
+
+static float	ray_h_collision(t_data *data, int ray)
 {
 	float	dist;
 
 	return (dist);
 }
-
-static float	ray_h_collision(t_data, int ray)
-{
-	float	dist;
-
-	return (dist);
-}
-
 
 void	ray_collision(t_data *data, int ray)
 {
